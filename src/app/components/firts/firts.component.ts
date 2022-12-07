0c55b7bd8f48c46074f55067f08b1ffbc427a18b
TawanMenezes <tawanmenezes@gmail.com>
2022-12-07 15:46:24 -0300

    interpolação_propiertes-biding

--- src/app/components/firts/firts.component.ts
+++ src/app/components/firts/firts.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-firts',
   templateUrl: './firts.component.html',
-  styleUrls: ['./firts.component.css']
+  styleUrls: ['./firts.component.css'],
 })
 export class FirtsComponent {
+  url: string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImg: string = 'http://lorempixel.com.br/400/200/nature';
+  constructor() {}
 
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
 }
